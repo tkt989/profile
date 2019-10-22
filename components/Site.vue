ab8565f6d186595f308d558a885eb443fd30e60b
tkt989 <d.0508taka@gmail.com>
2019-10-22 14:03:20 +0900

    initial commit

---
+++ components/Site.vue
@@ -0,0 +1,31 @@
+<template>
+  <a :href="url">{{title}}</a>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    url: String
+  }
+}
+</script>
+
+<style>
+a {
+  display: inline-block;
+  font-family: "Source Code Pro", monospace;
+  text-decoration: none;
+  font-weight: bold;
+  background-color: white;
+  color: black;
+  padding: 16px;
+  margin: 16px;
+  transition: filter 500ms;
+  filter: invert(100%);
+}
+
+a:hover {
+  filter: invert(0%);
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <site
+      v-for="site in sites"
+      :key="site.url"
+      :title="site.title"
+      :url="site.url"
+    ></site>
+  </div>
+</template>
+
+<script>
+import Site from '~/components/Site.vue'
+
+export default {
+  data: () => {
+    return {
+      sites: [
+        {
+          title: "Blog",
+          url: "https://blog.info.tkt989"
+        },
+        {
+          title: "Twitter",
+          url: "https://twitter.com/tkt989_dev"
+        },
+        {
+          title: "Github",
+          url: "https://github.com/tkt989"
+        }
+      ]
+    }
+  },
+  components: {
+    Site
+  }
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
